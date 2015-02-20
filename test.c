dc85552baee6cc0d029251d5425e0133780d29c6
James Titcumb <james@asgrim.com>
2015-02-20 16:06:55 +0000

    Added ./a.out hello and ./a.out add

--- test.c
+++ test.c
@@ -1,12 +1,35 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void usage(char* cmd, char* param, char* extra)
+{
+  printf("Usage: %s %s %s\n", cmd, param, extra);
+  return;
+}
 
 int main(int argc, char* argv[])
 {
   if (argc < 2) {
-    printf("Not enough args (there were %d)..\n", argc);
+    usage(argv[0], "[hello|add]", "");
     return 1;
   }
 
-  printf("Hello %s!\n", argv[1]);
+  if (strcmp(argv[1], "hello") == 0) {
+    if (NULL == argv[2]) {
+      usage(argv[0], argv[1], "[name]");
+      return 1;
+    }
+    printf("Hello %s!\n", argv[2]);
+  } else if (strcmp(argv[1], "add") == 0) {
+    if (NULL == argv[2] || NULL == argv[3]) {
+      usage(argv[0], argv[1], "[a] [b]");
+      return 1;
+    }
+    char *lol;
+    long a = strtol(argv[2], &lol, 10);
+    long b = strtol(argv[3], &lol, 10);
+    printf("The answer to %li + %li is %li.\n", a, b, (a+b));
+  }
   return 0;
 }
