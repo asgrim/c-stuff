8dfab3e033c823a8207a3fec236ef4eec3c0584d
James Titcumb <james@asgrim.com>
2015-03-03 09:36:55 +0000

    IO and function

--- test.cpp
+++ test.cpp
@@ -1,15 +1,27 @@
 #include <iostream>
 #include <string>
-
-#define HELLO_STR "Hello"
+#include <cstdlib>
 
 using namespace std;
 
+int perform_addition (int a, int b)
+{
+  return a + b;
+}
+
 int main()
 {
-  const string hello = HELLO_STR " world!";
+  string buf;
+  int a, b;
 
-  cout << hello << endl;
+  cout << "Please enter a: ";
+  getline(cin, buf);
+  a = atoi(buf.c_str());
+  cout << "Please enter b: ";
+  getline(cin, buf);
+  b = atoi(buf.c_str());
+
+  cout << "The result is: " << perform_addition(a, b) << endl;
 
   return 0;
 }
