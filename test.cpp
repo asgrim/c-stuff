4919c89d8752006e55ff7d632dd040d20de4b438
James Titcumb <james@asgrim.com>
2015-03-03 17:33:49 +0000

    Pointers

--- test.cpp
+++ test.cpp
@@ -1,28 +1,19 @@
 #include <iostream>
-#include <string>
-#include <cstdlib>
+#include <stdint.h>
 
 using namespace std;
 
-namespace foo
-{
-  template <class T>
-  inline T perform_addition (T a, T b)
-  {
-    T result;
-    result = a + b;
-    return result;
-  }
-}
-
 int main()
 {
-  string buf;
-  int a = 3, b = 5;
-  double c = 5.3, d = 1.6;
+  int v, w;
+  int *ptr = &v;
 
-  cout << "The first result is: " << foo::perform_addition(a, b) << endl;
-  cout << "The second result is: " << foo::perform_addition(c, d) << endl;
+  *ptr = 15;
+  ptr = &w;
+  *ptr = 10;
+
+  cout << "V is " << v << endl;
+  cout << "W is " << w << endl;
 
   return 0;
 }
