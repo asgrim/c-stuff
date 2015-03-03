b953cfc9ae941b8bc23f0986c06bc93a433ea178
James Titcumb <james@asgrim.com>
2015-03-03 12:32:48 +0000

    Function template

--- test.cpp
+++ test.cpp
@@ -4,24 +4,25 @@
 
 using namespace std;
 
-int perform_addition (int a, int b)
-{
-  return a + b;
-}
+template <class T>
+inline T perform_addition (T, T);
 
 int main()
 {
   string buf;
-  int a, b;
+  int a = 3, b = 5;
+  double c = 5.3, d = 1.6;
 
-  cout << "Please enter a: ";
-  getline(cin, buf);
-  a = atoi(buf.c_str());
-  cout << "Please enter b: ";
-  getline(cin, buf);
-  b = atoi(buf.c_str());
-
-  cout << "The result is: " << perform_addition(a, b) << endl;
+  cout << "The first result is: " << perform_addition(a, b) << endl;
+  cout << "The second result is: " << perform_addition(c, d) << endl;
 
   return 0;
+}
+
+template <class T>
+inline T perform_addition (T a, T b)
+{
+  T result;
+  result = a + b;
+  return result;
 }
